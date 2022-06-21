24850fe66acc9b9ffe14a0d986038003f9e51395
fidanmardanli <fidannm@code.edu.az>
2022-06-21 23:53:05 +0400

    Reversing the string

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            ReversingTask("Fidan muazzamdi");
+
+        }
+
+        static void ReversingTask(string word)
+        {
+            string reverseString = string.Empty;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverseString += word[i];
+            }
+            Console.Write($"Reverse String is : {reverseString} ");
+        }
+
+
+    }
+}
